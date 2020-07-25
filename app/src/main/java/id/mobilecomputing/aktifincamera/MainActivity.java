d2a7f700c149b3a8b0bea9930b5914d417c1929a
quirinusernest <rinusernest@gmail.com>
2020-07-25 12:53:58 +0700

    UASMOBILE2019

---
+++ app/src/main/java/id/mobilecomputing/aktifincamera/MainActivity.java
@@ -0,0 +1,101 @@
+package id.mobilecomputing.aktifincamera;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int PERMISSION_CODE = 1000;
+    private static final int IMAGE_CAPTURE_CODE =1001 ;
+
+    Button mCaptureBtn;
+    ImageView mImageView;
+
+    Uri image_uri;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mImageView = findViewById(R.id.image_view);
+        mCaptureBtn= findViewById(R.id.capture_image_btn);
+
+        //tombol klik
+        mCaptureBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //jika os =>marshmallow,minta runtime permission
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.
+                            PERMISSION_DENIED || checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) ==
+                            PackageManager.PERMISSION_DENIED) {
+                        String[] permission = {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+                        //Menampilkan pop up permintaan akses
+                        requestPermissions(permission, PERMISSION_CODE);
+                    }
+                    else {
+                        //akses already granted
+                        openCamera();
+                    }
+                }
+                else {
+                    //system os < marshmellow
+                    openCamera();
+                }
+            }
+        });
+    }
+    private void openCamera(){
+        ContentValues values = new ContentValues();
+        values.put(MediaStore.Images.Media.TITLE, "Gambar Terbaru");
+        values.put(MediaStore.Images.Media.DESCRIPTION, "Dari Kamera");
+        image_uri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,values);
+
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, image_uri);
+        startActivityForResult(cameraIntent, IMAGE_CAPTURE_CODE);
+    }
+
+    //handling akses
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case PERMISSION_CODE:{
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    openCamera();
+                }
+                else{
+                    Toast.makeText(this, "Akses Ditolak", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        //memanggil ketika gambar tercapture dari camera
+        if (resultCode == RESULT_OK) {
+            //menampilkan gambar pada image view
+            mImageView.setImageURI(image_uri);
+        }
+    }
+
+}
